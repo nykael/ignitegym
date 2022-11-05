dc0816beba60e53da3c2c4c5c0cc2e24554fb362
nykael <nykaelccardial@gmail.com>
2022-11-05 17:07:14 -0300

    exercise groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import { VStack, FlatList } from "native-base";
+
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack, Text } from "native-base";
 
 export function Home() {
+    const [groups, setGroups] = useState(['Costas', 'Bícips', 'Triceps', 'ombro'])
+    const [groupSelected, setGroupSelected] = useState('Costas')
+
    return(
     <VStack flex={1}>
         <HomeHeader />
+
+        <FlatList 
+          data={groups}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <Group 
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          _contentContainerStyle={{px: 8}}
+          my={10}
+          maxH={10}
+        />
     </VStack>
    ) 
 }
