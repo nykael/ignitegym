11c12f6cb7784406c3df2558c53a713f513dce44
nykael <nykaelccardial@gmail.com>
2022-11-07 11:55:17 -0300

    Loading Photo with Skeleton

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
+import { Center, ScrollView, VStack, Skeleton } from "native-base";
+
+
 import { ScreenHeader } from "@components/ScreenHeader";
-import { Center, VStack } from "native-base";
+import { UserPhoto } from "@components/UserPhoto";
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+    const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
    return(
     <VStack flex={1}>
         <ScreenHeader title="Perfil"/>
+          <ScrollView>
+            <Center mt={6} px={10}>
+                {
+                   photoIsLoading ?
+                     <Skeleton 
+                       w={PHOTO_SIZE}
+                       h={PHOTO_SIZE}
+                       rounded="full"
+                       startColor="gray.500"
+                       endColor="gray.400"
+                    />
+                    :
+                    <UserPhoto 
+                      size={PHOTO_SIZE}
+                      source={{uri: "http://github.com/nykael.png"}}
+                      alt="Foto do usuário"  
+                    />
+            }
+            </Center>
+        </ScrollView>
     </VStack>
    ) 
 }
