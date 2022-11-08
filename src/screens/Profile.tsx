4cb354d6714686f904c896e5aa8f8323ed73e65b
nykael <nykaelccardial@gmail.com>
2022-11-08 17:09:09 -0300

    Photor edit

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from "native-base";
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from "native-base";
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+
 
 
 import { ScreenHeader } from "@components/ScreenHeader";
@@ -12,6 +15,42 @@ const PHOTO_SIZE = 33
 
 export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false)
+    const [userPhoto, setUserPhoto] = useState('http://github.com/nykael.png')
+    const Toast = useToast()
+
+
+    async function handleUserPhotoSelect() {
+      setPhotoIsLoading(true)
+      try {
+        const photoSelected = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          quality: 1,
+          aspect: [4,4],
+          allowsEditing: true,
+         });
+  
+         if(photoSelected.cancelled) {
+            return
+         }
+
+         if(photoSelected.uri){
+           const photoInfo = await FileSystem.getInfoAsync(photoSelected.uri)
+          
+           if(photoInfo.size && (photoInfo.size /1024 /1024) > 5){
+            return Toast.show({
+              title: "Essa imagem é muito grande. Escolha uma de até 5MB",
+              placement: 'top',
+              bgColor: 'red.500'
+            })
+           }
+           setUserPhoto(photoSelected.uri)
+         }
+      } catch (error) {
+        console.log(error) 
+      }finally{
+        setPhotoIsLoading(false)
+      }
+    }
 
    return(
     <VStack flex={1}>
@@ -31,12 +70,12 @@ export function Profile() {
                     :
                     <UserPhoto 
                       size={PHOTO_SIZE}
-                      source={{uri: "http://github.com/nykael.png"}}
+                      source={{uri: userPhoto}}
                       alt="Foto do usuário"  
                     />
             }
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={handleUserPhotoSelect}>
                 <Text color="green.500" fontSize="md" mt={2}  mb={8}>
                     Alterar foto
                 </Text>
