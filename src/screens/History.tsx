7629d506e0ccc3c83c7949e05d691b12a0985759
nykael <nykaelccardial@gmail.com>
2022-12-13 09:39:15 -0300

    History

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { Center, Heading, VStack, SectionList, Text, useToast } from "native-base";
+import { Center, Heading, VStack, SectionList, Text, useToast  } from "native-base";
 
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
@@ -49,29 +49,34 @@ export function History() {
        <ScreenHeader title="Histórico de Exercícios" />
 
         {
-            isLoading ? <Loading /> : exercises?.length &&
-        <SectionList 
-            sections={exercises}
-            keyExtractor={item => item.id}
-            renderItem={({item}) => (
-                <HistoryCard 
-                    data={item}
-                /> 
-            )}
-            renderSectionHeader={({section}) => (
-                <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
-                    {section.title}
-                </Heading>
-            )}
-            px={8}
-            contentContainerStyle={exercises.length === 0 && {flex: 1, justifyContent: 'center'}}
-            ListEmptyComponent={() => (
-                <Text color="gray.100" textAlign="center">
+            isLoading ? <Loading /> : (
+
+                exercises?.length > 0 ?  <SectionList 
+                sections={exercises}
+                keyExtractor={item => item.id}
+                renderItem={({item}) => (
+                    <HistoryCard 
+                        data={item}
+                    /> 
+                )}
+                renderSectionHeader={({section}) => (
+                    <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+                        {section.title}
+                    </Heading>
+                )}
+                px={8}
+                contentContainerStyle={exercises.length === 0 && {flex: 1, justifyContent: 'center'}}
+            />
+
+            :
+                <Center flex={1}>
+                    <Text color="gray.100" textAlign="center">
                     Não há exercícios registrados ainda. {'\n'}
                     Vamos fazer exercícios hoje ?
-                </Text>
-            )}
-        />
+                    </Text>
+                </Center>
+            )
+        
         }
     </VStack>
    ) 
